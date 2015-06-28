50dbe582e8fd980e5a6bd1540b46c72ef13173cd
badomm <jimmy.chau.no@gmail.com>
2015-06-28 22:12:58 +0200

    Refactoring, collected inverter commands together

--- ecu_user_board/ecu_user_board/src/ECU_CAN/ecu_can_messages.h
+++ ecu_user_board/ecu_user_board/src/ECU_CAN/ecu_can_messages.h
@@ -8,14 +8,21 @@
 
 #ifndef ECU_CAN_MESSAGES_H_
 #define ECU_CAN_MESSAGES_H_
-
+////////////////////////////
+// Inverter Messages
+///////////////////////////
 void ecu_can_send_to_inverter(uint8_t inverter_reg, uint16_t data);
 void ecu_can_inverter_torque_cmd(int16_t torque);
 void ecu_can_inverter_read_reg(uint8_t inverter_reg);
-void ecu_can_send_to_dash(uint16_t data);
-void ecu_can_send_tractive_system_active(void);
 void ecu_can_inverter_enable_drive(void);
 void ecu_can_inverter_disable_drive(void);
+void ecu_can_inverter_read_torque_periodic(void);
+
+//////////////////////////////////
+//Dash messages
+////////////////////////////////
+void ecu_can_send_to_dash(uint16_t data);
+void ecu_can_send_tractive_system_active(void);
 void ecu_can_send_current_bms(uint16_t current);
 void ecu_can_send_slow_data(uint16_t motor_temp, uint16_t inverter_temp, uint8_t max_trq);
 void ecu_can_send_fast_data(uint16_t inverter_vdc, uint16_t ecu_error, uint16_t rpm, int16_t trq_cmd);
@@ -26,6 +33,6 @@ void ecu_can_send_drive_disabled(void);
 void ecu_can_confirm_activate_launch(void);
 void ecu_can_send_launch_ready(void);
 void ecu_can_send_launch_stop(void);
-void ecu_can_inverter_read_torque_periodic(void);
+
 
 #endif /* ECU_CAN_MESSAGES_H_ */
