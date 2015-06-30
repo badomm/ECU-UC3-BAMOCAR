cf0ff5a75ab5965d0d14c9329af40e00eb3ef63c
badomm <jimmy.chau.no@gmail.com>
2015-06-30 19:57:12 +0200

    added float endianSwapper

--- ecu_user_board/ecu_user_board/src/endianSwapper.h
+++ ecu_user_board/ecu_user_board/src/endianSwapper.h
@@ -11,6 +11,6 @@
 
 #define endianSwapperU32(num) (((num>>24)&0xff) | ((num<<8)&0xff0000) | ((num>>8)&0xff00) | ((num<<24)&0xff000000)) 
 
-
+float endianSwapperF(float num);
 
 #endif /* ENDIANSWAPPER_H_ */
